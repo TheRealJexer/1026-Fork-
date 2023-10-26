1268ee274609ad9fa99f439319d8f1207a82c184
Bob <bob@gmail.com>
2023-10-26 16:04:38 -0600

    Learning how files work in python

--- ex1.py
+++ ex1.py
@@ -1,7 +1,9 @@
 print('Opening a txt file for writing')
 f_obj = open('test.txt', mode='w')
+f_obj.write("hi")
 print('Opening a txt file for reading')
 f_obj = open('test.txt', mode='r')
 
 # uncomment below, will result in error
-# f_obj.write("hi")
+# move .write function above the mode = r function and it will work
+# Remember. Python reads form top to bottom!
